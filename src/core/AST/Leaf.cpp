f07087ec11b71c630fa40f290b0b728401f836f4
Garry Morrison <garry@semantic-db.org>
2023-09-29 15:37:40 +0930

    very small tidy to Leaf.cpp

--- src/core/AST/Leaf.cpp
+++ src/core/AST/Leaf.cpp
@@ -6,19 +6,11 @@
 
 namespace SKet {
 
-	/*
 	Leaf::Leaf(yyTOKEN token) // Move to header file?
 		: tok(token)
 	{
 		id = serial.get_id();
 		ntype = NodeType::Leaf;
 	}
-	*/
-	Leaf::Leaf(yyTOKEN token)
-		// : AST(serial.get_id())
-	{
-		id = serial.get_id();
-		tok = token;
-		ntype = NodeType::Leaf;
-	}
+
 };
