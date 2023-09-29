670ac33ae0bf7f8591af61c02e8e0f213ebb34cf
Garry Morrison <garry@semantic-db.org>
2023-09-29 15:24:04 +0930

    Fixed level bug in PrintTree

--- src/core/ASTVisitor/PrintTree.h
+++ src/core/ASTVisitor/PrintTree.h
@@ -2,7 +2,7 @@
 
 // Author: Garry Morrison
 // Added: 2023-9-28
-// Updated: 2023-9-28
+// Updated: 2023-9-29
 
 #include <iostream>
 #include "../AST/Leaf.h"
@@ -21,25 +21,12 @@ namespace SKet {
 		PrintTree() {}
 
 		virtual void visit(Leaf& Node) override {
-			// std::cout << "PrintTree inside Leaf\n";
-			std::cout << indent(2 * level) << Node.id << "  " << Node.tok.text << " (" << Node.tok.type_to_string() << "): line " << Node.tok.line << ", column " << Node.tok.col << "\n";
+			std::cout << indent(3 * level) << Node.id << "  " << Node.tok.text << " (" << Node.tok.type_to_string() << "): line " << Node.tok.line << ", column " << Node.tok.col << "\n";
 		}
 
 		virtual void visit(Internal& Node) override {
-			// std::cout << "PrintTree inside Internal\n";
-			/*
-			if (Node.sym.empty())
-			{
-				std::cout << indent(2 * level) << Node.id << "  Node Type: " << node_type.Name(Node.ntype) << ": children: " << Node.nkids << "\n";
-			}
-			else
-			{
-				std::cout << indent(2 * level) << Node.id << "  Node Type: " << Node.sym << " (" << Node.rule_id << "): children: " << Node.nkids << "\n";
-			}
-			*/
-
 			// std::cout << indent(2 * level) << Node.id << "  Node Type: " << Node.type_to_string() << ": children: " << Node.nkids << "\n";
-			std::cout << indent(2 * level) << Node.id << "  " << Node.type_to_string() << ": children: " << Node.nkids << "\n";
+			std::cout << indent(3 * level) << Node.id << "  " << Node.type_to_string() << ": children: " << Node.nkids << "\n";
 
 			level++;
 			for (AST* tree : Node.kids)
@@ -49,6 +36,7 @@ namespace SKet {
 					tree->accept(*this);
 				}
 			}
+			level--;
 		}
 
 		virtual void visit(Root& Node) override { visit(static_cast<Internal&>(Node)); }
