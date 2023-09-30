371f74072d834f5596b8e253b5cb2f28d969bf02
Garry Morrison <garry@semantic-db.org>
2023-09-30 15:50:44 +0930

    a small tidy of SymbolTable

--- src/core/SymTable/SymTable.cpp
+++ src/core/SymTable/SymTable.cpp
@@ -4,3 +4,66 @@
 
 #include "SymTable.h"
 
+void SymbolTable::initTypeSystem() {  // Add more types here later!
+	define(new BuiltInType("ket"));
+	define(new BuiltInType("sp"));
+	define(new BuiltInType("seq"));
+}
+
+std::string SymbolTable::getScopeName() {
+	if (name.empty())
+	{
+		return "global"; // default scope name is "global"
+	}
+	else
+	{
+		return name;
+	}
+}
+
+void SymbolTable::define(Symbol* sym) {
+	if (sym)
+	{
+		if (!sym->name.empty())
+		{
+			symbols[sym->name] = sym;
+		}
+	}
+}
+Symbol* SymbolTable::resolve(const std::string& name)
+{
+	if (symbols.find(name) == symbols.end())  
+	{
+		Scope* parent = getEnclosingScope(); // look in parent scope too! More complex implementation later
+		if (parent)
+		{
+			return parent->resolve(name);
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+	else
+	{
+		return symbols[name];
+	}
+}
+
+std::string SymbolTable::to_string(int level) {
+	std::string s;
+	s = indent(2 * level) + getScopeName() + ":\n";
+	for (const auto& elt : symbols)
+	{
+		if (elt.second)
+		{
+			s += indent(2 * level + 2) + elt.second->to_string() + "\n";
+		}
+	}
+	Scope* parent = getEnclosingScope();
+	if (parent)
+	{
+		s += parent->to_string(level + 1);
+	}
+	return s;
+}
--- src/core/SymTable/SymTable.h
+++ src/core/SymTable/SymTable.h
@@ -29,64 +29,16 @@ public:
 	Scope* parent_scope;
 	std::map<std::string, Symbol*> symbols;
 	SymbolTable() { initTypeSystem(); }
-	SymbolTable(const std::string& name) { this->name = name; }
-	SymbolTable(const std::string& name, Scope* scope) { this->name = name; this->parent_scope = scope; }
-	void initTypeSystem() {
-		define(new BuiltInType("ket"));
-		define(new BuiltInType("sp"));
-		define(new BuiltInType("seq"));
-	}
+	SymbolTable(const std::string& name) { this->name = name; }  // only empty constructor initiates the type system
+	SymbolTable(const std::string& name, Scope* scope) { this->name = name; this->parent_scope = scope; } // ditto
+	void initTypeSystem();
 
 	// satisfy scope interface:
-	std::string getScopeName() { 
-		if (name.empty())
-		{
-			return "global"; // default scope name is "global"
-		}
-		else
-		{
-			return name;
-		}
-	}
+	std::string getScopeName(); 
 	Scope* getEnclosingScope() { return parent_scope; }
-	void define(Symbol *sym) { 
-		if (sym)
-		{
-			if (!sym->name.empty())
-			{
-				symbols[sym->name] = sym;
-			}
-		}
-	}
-	Symbol* resolve(const std::string& name)
-	{
-		if (symbols.find(name) == symbols.end())  // look in parent scope too!
-		{
-			return nullptr;
-		}
-		else
-		{
-			return symbols[name];
-		}
-	}
-
+	void define(Symbol* sym); 
+	Symbol* resolve(const std::string& name);
 	std::string to_string() { return this->to_string(1); }
+	std::string to_string(int level);
 
-	std::string to_string(int level) {
-		std::string s;
-		s = indent(2 * level) + getScopeName() + ":\n";
-		for (const auto& elt : symbols)
-		{
-			if (elt.second)
-			{
-				s += indent(2 * level + 2) + elt.second->to_string() + "\n";
-			}
-		}
-		Scope *parent = getEnclosingScope();
-		if (parent)
-		{
-			s += parent->to_string(level + 1);
-		}
-		return s;
-	}
 };
